bfd8f9d2c809f3ea2cfb2c78ffc748c332a8884c
Luka Panio <lukapanio@gmail.com>
2024-02-16 11:32:22 +0100

    dm3q: Initial rebrand
    Change-Id: I4769406714f1482fb00d3db3dec67a1c392cb552

--- Android.mk
+++ Android.mk
@@ -11,10 +11,10 @@ ifneq ($(filter dm3q,$(TARGET_DEVICE)),)
 WIFI_FIRMWARE_SYMLINKS := $(TARGET_OUT_VENDOR)/firmware/
 $(WIFI_FIRMWARE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Creating WiFi firmware symlinks: $@"
-	@mkdir -p $@/wlan/qca_cld/qca6490
+	@mkdir -p $@/wlan/qca_cld/kiwi_v2
 	$(hide) ln -sf /data/vendor/firmware/wlanmdsp.mbn $@/wlanmdsp.otaupdate
-	$(hide) ln -sf /mnt/vendor/persist/qca6490/wlan_mac.bin $@/wlan/qca_cld/qca6490/wlan_mac.bin
-	$(hide) ln -sf /vendor/etc/wifi/qca6490/WCNSS_qcom_cfg.ini $@/wlan/qca_cld/qca6490/WCNSS_qcom_cfg.ini
+	$(hide) ln -sf /mnt/vendor/persist/kiwi_v2/wlan_mac.bin $@/wlan/qca_cld/kiwi_v2/wlan_mac.bin
+	$(hide) ln -sf /vendor/etc/wifi/kiwi_v2/WCNSS_qcom_cfg.ini $@/wlan/qca_cld/kiwi_v2/WCNSS_qcom_cfg.ini
 
 ALL_DEFAULT_INSTALLED_MODULES += \
     $(WIFI_FIRMWARE_SYMLINKS)
